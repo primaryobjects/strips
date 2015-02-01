82679f6549762bcc29b9be00621a001840082987
Kory Becker <kbecker@primaryobjects.com>
2015-01-31 21:47:54 -0500

    Refactored to return solution path.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var strips = require('strips');
+var strips = require('./lib/strips');
 
 // Load the domain and problem.
-strips.load('./grammar/blocksworld3/domain.txt', './grammar/blocksworld3/problem.txt', function(domain, problem) {
+strips.load('./grammar/blocksworld3/domain.txt', './grammar/blocksworld3/problem2.txt', function(domain, problem) {
     // Run the problem against the domain.
-    strips.solve(domain, problem);
+    var path = strips.solve(domain, problem);
+    if (path) {
+        console.log('* Solution found in ' + path.steps + ' steps!');
+        for (var i in path) {
+            console.log(path[i].step + '. ' + path[i].action);
+        }
+    }
 });
