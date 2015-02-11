d10a7840aae462f9552bbaa28d0b90290fbcdfdd
Kory Becker <kbecker@primaryobjects.com>
2015-02-10 23:01:07 -0500

    Updated readme.

--- strips/Readme.md
+++ strips/Readme.md
@@ -160,7 +160,17 @@ console.log('New child state');
 console.log(strips.stateToString(childState));
 ```
 
-Of course, the real power is in designing your own search algorithm using the strips methods. See the [default](https://github.com/primaryobjects/strips/blob/master/strips/strips.js#L440) search routine for an idea of how to use the methods to search. Have fun!
+Of course, the real power is in designing your own search algorithm using the strips methods. See the [default](https://github.com/primaryobjects/strips/blob/master/strips/strips.js#L440) search routine for an idea of how to use the methods to search or read below for a concrete example.
+
+### A* Search
+
+You can implement your own A* search to find a solution. A* works by using a heuristic to guide it down the path of possible moves in the domain. In this manner, it is much faster than simple breadth-first or depth-first search. It will also find an optimal solution that contains the least number of steps.
+
+Since the strips library exposes its internal methods, you can implement your own search algorithm. Here is [example](https://github.com/primaryobjects/strips/blob/master/strips/astar-example.js) of an A* search method for the [Starcraft](https://github.com/primaryobjects/strips/blob/master/examples/starcraft/domain.txt) domain, to train a [Marine](https://github.com/primaryobjects/strips/blob/master/examples/starcraft/marine.txt).
+
+The core idea to a custom search method is to use the strips methods isGoal() and getChildStates() to iterate through all states and actions. Once you have a list of child states, apply your heuristic to calculate a cost for each state. Then sort the states by cost so that A* can choose the next cheapest state to move to. You can see the details in the [example](https://github.com/primaryobjects/strips/blob/master/strips/astar-example.js).
+
+Have fun!
 
 License
 -------
---
+++ strips/astar-example.js
@@ -0,0 +1,95 @@
+var strips = require('./strips');
+strips.verbose = true;
+
+// Load the domain and problem.
+strips.load('../examples/starcraft/domain.txt', '../examples/starcraft/marine.txt', function(domain, problem) {
+    // Run the problem against the domain.
+    var solutions = solveA(domain, problem);
+
+    // Display solution.
+    var solution = solutions[0];
+    console.log('- Solution found in ' + solution.steps + ' steps!');
+    for (var i = 0; i < solution.path.length; i++) {
+        console.log((i + 1) + '. ' + solution.path[i]);
+    }        
+});
+
+function costMarine(state) {
+    // Calculates the A* cost of a state. For Starcraft, the heuristic will be how many required buildings have been built. Subtract 1 from cost for each correct building, with 0 meaning all required buildings have been made and we're done.
+    var cost = 10;
+
+    for (var i in state.actions) {
+        var action = state.actions[i].action;
+
+        if (action == 'depot') {
+            cost -= 5;
+        }
+        else if (action == 'barracks') {
+            cost -= 5;
+        }
+    }
+    
+    return cost;
+}
+
+function solveA(domain, problem) {
+    // Find first solution using A* search.
+    var state = problem.states[0];
+    var goalState = problem.states[1];
+    var depth = 0;
+    var fringe = [ { state: state, h: costMarine(state), g: depth } ]; // Start with the initial state on the fringe.
+    var visited = {};
+    var solutions = [];
+
+    while (fringe.length > 0) {
+        // Investigate the next state with the lowest cost.
+        var current = fringe[0];
+
+        // Remove this state from the fringe.
+        fringe.shift();
+
+        // Mark this state as visited.
+        visited[strips.stateToString(current.state)] = 1;
+
+        // Check for goal.
+        if (strips.isGoal(current.state, goalState)) {
+            // Compile solution path.
+            var path = [];
+            var steps = current.g;
+
+            while (current != null && current.parent != null) {
+                // Since we move from goal backwards, add this step to the front of the array (rather than the end, otherwise it would be in reverse order).
+                path.unshift(strips.actionToString(current.action));
+                current = current.parent;
+            }
+
+            solutions.push({ steps: steps, path: path });
+
+            return solutions;
+        }
+        else {
+            // Get child states by applying actions to current state.
+            var children = strips.getChildStates(domain, current.state);
+
+            // Add the children to the fringe.
+            for (var i in children) {
+                var child = children[i];
+                child.parent = current;
+                child.g = current.g + 1;
+                child.h = costMarine(child.state);
+                
+                if (!visited[strips.stateToString(child.state)]) {
+                    fringe.push(child);
+                }
+            }
+            
+            fringe.sort(function(a, b) { return (a.h + a.g) - (b.h + b.g) });
+        }
+
+        if (strips.verbose) {
+            console.log('Depth: ' + current.g + ', Current cost: ' + (current.h + current.g) + ', ' + fringe.length + ' child states.');
+        }
+    }
+
+    return solutions;
+}
