b6c72947beb2144eec864bd0676ec13e74ff4e0f
Kory Becker <kbecker@primaryobjects.com>
2018-01-16 11:55:35 -0500

    Fixed async deprecation.

--- graph.js
+++ graph.js
@@ -1,9 +1,9 @@
 /*
 Example Planning Graph - AI Planning with STRIPS and PDDL.
 Builds a planning graph for a given domain and problem. Renders the graph using d3.js and saves to a file graph.svg.
-Run using: node --harmony graph
+Run using: node graph
 
-Copyright (c) 2017 Kory Becker
+Copyright (c) 2018 Kory Becker
 http://primaryobjects.com/kory-becker
 
 License MIT
@@ -46,7 +46,7 @@ function getTreeData(graph, layerIndex) {
         var action = layer[j];
 
         // Format action name: 'cook x y z'.
-        var name = action.action + '-';
+        var name = action.action + (action.parameters ? '-' : '');
         for (var k in action.parameters) {
             name += action.parameters[k].parameter + ' ';
         }
@@ -315,7 +315,7 @@ function saveGraph(d3, el, fileName) {
     //var svgXML = (new xmldom.XMLSerializer()).serializeToString(svgGraph);
     svgXML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">' + svgGraph + '</svg>';
 
-    fs.writeFile(fileName, svgXML);
+    fs.writeFile(fileName, svgXML, err => { if (err) console.error(err); });
 
     console.log('Saved ' + fileName);
 }
